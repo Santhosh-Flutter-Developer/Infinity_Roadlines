ee1a5e574a0b45a639b9ead4565443a1294a10ee
srisoftwarez-projects <msankar@srisoftwarez.com>
2026-07-14 18:21:58 +0530

    feat: google map key added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Required by google_maps_flutter on iOS. Must be called before Flutter/GoogleMaps
+    // is used, or the map will fail to render (and can crash) at runtime.
+    GMSServices.provideAPIKey("AIzaSyDlDoDtlSG5pxOpWQ2KrS6xjYjfqMmCyEs")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
